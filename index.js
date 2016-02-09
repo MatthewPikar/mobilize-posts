41fadeda725b9f0adace0ac01b709d57b2b49eb2
matthew.pikar <matt.piekarczyk@cortixsystems.com>
2016-02-09 12:41:34 -0500

    first version

---
+++ index.js
@@ -0,0 +1,35 @@
+/**
+ * Created by mattpiekarczyk on 11/4/15.
+ */
+"use strict"
+
+var _ = require('lodash')
+var options = {}
+
+for(var i= 2, len=process.argv.length; i<len; i++){
+    var argument = process.argv[i].split(':')
+    options[argument[0]] = argument[1]
+}
+
+_.extend(options, {
+    resourceName: 'posts',
+    resourceFormat: {
+        required$: ['sourceId','title'],
+        only$: ['id','sourceId','title','date','content'],
+        id: 'string$',
+        sourceId: 'string$',
+        title: 'string$',
+        date: 'string$',
+        content: 'string$'
+    }
+})
+
+var resourceService = require('resource-service')
+require('seneca')()
+    .use('redis-transport')
+    .use(resourceService, options)
+    .listen({type:'redis', pin:'role:posts,cmd:get'})
+    .listen({type:'redis', pin:'role:posts,cmd:query'})
+    .listen({type:'redis', pin:'role:posts,cmd:add'})
+    .listen({type:'redis', pin:'role:posts,cmd:modify'})
+    .listen({type:'redis', pin:'role:posts,cmd:delete'})
